c369cdc70655fb227240ccee62c3335db661073d
Mike Babetchki <mike.babetchki@gmail.com>
2020-05-15 17:23:07 -0500

    updated code structure/refactoring

--- app.js
+++ app.js
@@ -6,10 +6,22 @@ const toggleAllBtn = document.getElementById("toggle-all");
 var todos = [];
 
 //Event Listeners
-toggleAllBtn.addEventListener("click", toggleAll);
+toggleAllBtn.addEventListener("click", function (e) {
+	if (e.target.id === "toggle-all") {
+		toggleAll(e);
+	}
+});
 addTodoBtn.addEventListener("click", addTodos);
-displaySection.addEventListener("click", deleteTodo);
-displaySection.addEventListener("click", toggleCompleted);
+displaySection.addEventListener("click", function (e) {
+	if (e.target.classList[0] === "delete-button") {
+		deleteTodo(e);
+	}
+});
+displaySection.addEventListener("click", function (e) {
+	if (e.target.classList[0] === "toggle") {
+		toggleCompleted(e);
+	}
+});
 displaySection.addEventListener("click", function (e) {
 	if (e.target.className === "item-li") {
 		editTodo(e);
@@ -26,6 +38,11 @@ displaySection.addEventListener("focusout", function (e) {
 		e.target.classList.add("hidden");
 	}
 });
+displaySection.addEventListener("keyup", function (e) {
+	if (event.which == 13 || event.keyCode == 13) {
+		e.target.blur();
+	}
+});
 displaySection.addEventListener("click", function (e) {
 	if (e.target.className === "branch-button") {
 		createBranch(e);
@@ -37,7 +54,7 @@ displaySection.addEventListener("click", function (e) {
 function addTodos(e) {
 	e.preventDefault();
 	var todoText = inputText.value;
-	var id = todos.length;
+	var id = todos.length + "";
 
 	todos.push({
 		name: todoText,
@@ -52,30 +69,32 @@ function addTodos(e) {
 
 function deleteTodo(e) {
 	e.preventDefault();
-	var item = e.target;
 
-	if (item.classList[0] === "delete-button") {
-		var parent = item.parentElement;
-		var index = parent.id - 1;
-		todos.splice(index, 1);
-		parent.remove();
-	}
+	todos.forEach(function (todo, i, todos) {
+		if (todos[i].id === e.target.parentElement.id) {
+			todos.splice(i, 1);
+		}
+	});
+
+	displayTodos();
 }
 
 //Toggle Todo as Completed
 
 function toggleCompleted(e) {
-	var item = e.target;
-	var parent = item.parentElement;
-
-	if (item.classList[0] === "toggle") {
-		var index = parent.id - 1;
-		parent.classList.add("completed");
-		todos[index].completed = true;
-	}
+	// debugger;
+	todos.forEach(function (todo, i, todos) {
+		if (todos[i].id === e.target.parentElement.id) {
+			todo.completed = !todo.completed;
+		}
+	});
+	displayTodos();
 }
 
+// Toggle All
+
 function toggleAll(e) {
+	// debugger;
 	e.preventDefault();
 	var counter = 0;
 	todos.forEach(function (todo) {
@@ -83,20 +102,17 @@ function toggleAll(e) {
 			counter++;
 		}
 	});
-
-	todos.forEach(function (todo) {
-		if (counter === todos.length) {
+	if (counter === todos.length) {
+		todos.forEach(function (todo) {
 			todo.completed = false;
-			var id = (todo.id + 1).toFixed();
-			var divItem = document.getElementById(id);
-			divItem.classList.remove("completed");
-		} else if (todo.completed === false) {
+		});
+	} else {
+		todos.forEach(function (todo) {
 			todo.completed = true;
-			var id = (todo.id + 1).toFixed();
-			var divItem = document.getElementById(id);
-			divItem.classList.add("completed");
-		}
-	});
+		});
+	}
+
+	displayTodos();
 }
 
 function editTodo(e) {
@@ -111,44 +127,49 @@ function editTodo(e) {
 	inputField.focus();
 }
 
-function createBranch(e) {
-	e.preventDefault();
-}
+function createBranch(e) {}
 
 //Display Todos to the screen
 
 function displayTodos() {
-	var branchBtn = document.createElement("button");
-	var todoText = inputText.value;
-	var todoLi = document.createElement("li");
-	var todoItem = document.createElement("div");
-	var deleteBtn = document.createElement("button");
-	var checkBtn = document.createElement("button");
-	var editInput = document.createElement("input");
+	displaySection.innerHTML = "";
 
-	editInput.classList.add("hidden");
-	editInput.classList.add("edit-input");
+	todos.forEach(function (todo) {
+		var branchBtn = document.createElement("button");
+		var todoLi = document.createElement("li");
+		var todoItem = document.createElement("div");
+		var deleteBtn = document.createElement("button");
+		var checkBtn = document.createElement("button");
+		var editInput = document.createElement("input");
+		var todoText = todo.name;
+		branchBtn.innerHTML = '<i class="fas fa-code-branch"></i>';
+		branchBtn.classList.add("branch-button");
 
-	branchBtn.innerHTML = '<i class="fas fa-code-branch"></i>';
-	branchBtn.classList.add("branch-button");
+		editInput.classList.add("hidden");
+		editInput.classList.add("edit-input");
 
-	deleteBtn.innerHTML = '<i class="fas fa-minus-circle"></i>';
-	deleteBtn.classList.add("delete-button");
+		deleteBtn.innerHTML = '<i class="fas fa-minus-circle"></i>';
+		deleteBtn.classList.add("delete-button");
 
-	checkBtn.innerHTML = '<i class="far fa-check-circle"></i>';
-	checkBtn.classList.add("toggle");
+		checkBtn.innerHTML = '<i class="far fa-check-circle"></i>';
+		checkBtn.classList.add("toggle");
 
-	todoLi.innerHTML = todoText;
-	todoLi.classList.add("item-li");
+		todoLi.classList.add("item-li");
+		todoItem.classList.add("item");
 
-	todoItem.classList.add("item");
-	todoItem.id = todos.length;
+		if (todo.completed === true) {
+			todoItem.classList.add("completed");
+		}
 
-	todoItem.appendChild(branchBtn);
-	todoItem.appendChild(editInput);
-	todoItem.appendChild(todoLi);
-	todoItem.appendChild(deleteBtn);
-	todoItem.appendChild(checkBtn);
-	displaySection.appendChild(todoItem);
-	inputText.value = "";
+		todoItem.id = todo.id;
+		todoLi.innerHTML = todoText;
+		todoItem.appendChild(branchBtn);
+		todoItem.appendChild(editInput);
+		todoItem.appendChild(todoLi);
+		todoItem.appendChild(deleteBtn);
+		todoItem.appendChild(checkBtn);
+		displaySection.appendChild(todoItem);
+
+		inputText.value = "";
+	});
 }
